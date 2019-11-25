3adf71451a42c7094f7572fe720104fb62e22729
Ramedlav <ochev.vladimir@gmail.com>
2019-11-25 14:51:38 +0100

    setVar in categories

--- trening/app/controllers/MenuController.php
+++ trening/app/controllers/MenuController.php
@@ -4,7 +4,7 @@ class MenuController extends  \Phalcon\Mvc\Controller
 
     public function indexAction()
     {
-        $category = Categories::findFirst();//$this->modelsManager->executeQuery('Select * FROM categories');
+//        $category = Categories::findFirst();//$this->modelsManager->executeQuery('Select * FROM categories');
 
         $this->request->isPost('categories' = 'werefi');//$category->name);
     }
